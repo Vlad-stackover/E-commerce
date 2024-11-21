38679d6ae4bdd6e25457dbb2c5bc150ae9652732
Vlad <84966677+Vlad-stackover@users.noreply.github.com>
2024-11-21 21:48:55 +0100

    Added stripe
    Working on purchase page,
    stripe and product.imagePath was added

--- ecommerce/src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ ecommerce/src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,8 +1,9 @@
 import db from "@/db/db"
 import { notFound } from "next/navigation"
 import Stripe from "stripe"
+import { CheckoutForm } from "./_components/CheckoutForm"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({
     params: {id},
@@ -13,5 +14,16 @@ export default async function PurchasePage({
         where: { id }
     })
     if (product == null) return notFound()
-    return <h1>Buy Product</h1>
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: product.priceInCents,
+        currency: "PLN",
+        metadata: { productId: product.id }
+    })
+
+    if(paymentIntent.client_secret == null){
+        throw Error("Stripe failed to create payment intent")
+    }
+    return <CheckoutForm product={product}
+    clientSecret={paymentIntent.client_secret}/>
 }
